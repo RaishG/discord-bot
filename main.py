5f1d9fd086a437b49dc0cfbd8a4b2e8d4bf2c358
RaishG <mraishghoffaryansyah@gmail.com>
2025-05-23 20:07:06 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import commands
+import random
+import os
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='#', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def kali(ctx, a: int , b: int):
+    await ctx.send(a * b)
+
+@bot.command()
+async def meme(ctx):
+    img_name = random.choice(os.listdir('discord/images'))
+    with open(f'discord/images/{img_name}', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('jenis-sampah')
+async def durations(ctx):
+    with open('discord/images/Jenis-Sampah.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+
+bot.run("MTM2NTI5MzQ3MDI2NDA3MDE3NQ.GNEAUU.7B_y8fiKRBD0MhYN1nt8haHNx6kS4HdZVIdmHs")
+
